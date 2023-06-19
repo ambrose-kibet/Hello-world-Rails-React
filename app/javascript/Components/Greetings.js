2237418c0d2670b15a932fa20c9725dbc62eaf5e
ambrose-kibet <ambrosekibet36@gmail.com>
2023-06-20 00:34:42 +0300

    add redux

--- app/javascript/Components/Greetings.js
+++ app/javascript/Components/Greetings.js
@@ -1,16 +1,22 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getGreeting } from '../redux/features/greetingSlice';
 
 const Greetings = () => {
-  const [data, setData] = useState(null)
-  const getGreeting = async() => {
-   await axios("/greetings").then(res=>setData(res.data)).catch(err=>console.log(err))
+  const dispatch = useDispatch();
+  const { isLoading, greeting } = useSelector((state) => state.greetings);
+  useEffect(() => {
+    dispatch(getGreeting());
+  }, []);
+  if (isLoading) {
+    return <h1 style={{ textAlign: 'center' }}>Loading...</h1>;
   }
+
   return (
     <div>
-      <h1>Hello world</h1>
-      <button>change greeting</button>
+      <h1>{(greeting && greeting) || 'Hello world'}</h1>
+      <button onClick={() => dispatch(getGreeting())}>change greeting</button>
     </div>
-  )
-}
-export default Greetings
+  );
+};
+export default Greetings;
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,20 +1,27 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import '@hotwired/turbo-rails';
+import './controllers';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Greetings from "./Components/Greetings";
-import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import Greetings from './Components/Greetings';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 function App() {
-  return
-  <Router>
-    <Routes>
-      <Route path="/" element={<Greetings />}/>
-    </Routes>
-  </Router>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Greetings />} />
+      </Routes>
+    </Router>
+  );
 }
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
