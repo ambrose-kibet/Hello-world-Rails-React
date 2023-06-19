cbfb43c75390929e82fba173f0a131492a4fc004
Ambrose Kibet <ambrosekibet36@gmail.com>
2023-06-20 01:05:44 +0300

    Update greetings_controller.rb

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,7 +1,7 @@
 class GreetingsController < ApplicationController
   def index
     @messages = Greeting.all
-    random_number = rand(1..@messages.length - 1)
+    random_number = rand(0..@messages.length - 1)
     render json: @messages[random_number]
   end
 end
